036cb9cf160a14b6da3e9856a2cd74e59ec5d784
berkleylim <berkleylim16@gmail.com>
2022-11-19 16:45:44 +0900

    react + spring setting

---
+++ jwtback/src/main/java/com/example/jwtback/JwtbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtbackApplication.class, args);
+	}
+
+}
